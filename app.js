ae5a40efe45f229791cd33b91f9faae82dcb71c5
spencershell <spencer@spencer-shell.com>
2015-07-26 15:34:22 -0400

    new fix

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/// <reference path="typings/node/node.d.ts"/>
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,25 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var http = require('http');
-
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
-var store = require('./routes/store');
-
 var app = express();
 
-// get routes
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + "/views/index.html"); 
-// });
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
